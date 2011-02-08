0520f6f69f70e02f2fb978c15c2752c21376bf69
roshanpersistent <roshan_devadiga@persistent.co.in>
2011-02-08 10:23:01 +0530

    email setting

--- config/environment.rb
+++ config/environment.rb
@@ -45,7 +45,7 @@ end
 require 'tlsmail'
 ActionMailer::Base.default_content_type = "text/html"
 Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-ActionMailer::Base.delivery_method = :test
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
 
